e949a365b651232c71c9e52dac3517b576e1ee22
Dineshhhh-Kumar <dineshkumar.p.6494@gmail.com>
2024-12-04 13:50:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN //(not a number) type=>number
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 =>true; 0 => false
+//"" => false
+//"Dinesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringnumber);
+console.log(typeof stringNumber);
+
+
