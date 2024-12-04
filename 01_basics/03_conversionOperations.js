1b46bbdef019884e2577cc02d06218f4e8575c46
Dineshhhh-Kumar <dineshkumar.p.6494@gmail.com>
2024-12-04 17:32:12 +0000

    done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,3 +29,55 @@ console.log(stringnumber);
 console.log(typeof stringNumber);
 
 
+// *********************Operations************************
+
+let value = 3
+let negvalue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1="hello"
+let str2=" Dinesh"
+
+let str3 =str1 + str2
+console.log(str3);
+
+console.log("1" +2);       //12
+console.log(1 +"2");       //12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 +"2");   //32  
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++;            
+                             /*let x = 3;
+                             const y = x++;
+                             console.log(`x:${x}, y:${y}`);
+                             // Expected output: "x:4, y:3"
+                             let a = 3;
+                             const b = ++a;
+                             console.log(`a:${a}, b:${b}`);
+                             // Expected output: "a:4, b:4"
+                             */
+
+console.log(gameCounter);
+
+
+
+
+
+
+
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive
+     // 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;   //undefined
+
+const id=Symbol('123')
+const anotherId=Symbol('123')  //false
+
+const bigNumber =46548787845353687845535554546n
+
+//Referance (Non primitive)
+     //Array, Object, Functions
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+     name:"Dinesh",
+     age:22,
+
+}
+
+const myFunction = function() {
+     console.log("Hello World");
+     
+}
