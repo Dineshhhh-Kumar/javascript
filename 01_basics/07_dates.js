42fa2e264d2a1ce4db0baca96db22604ef36f2eb
Dineshhhh-Kumar <dineshkumar.p.6494@gmail.com>
2024-12-11 03:11:50 +0000

    done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,19 @@
+//  Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
+
+let myNewDate = new Date("2023-01-14")
+let Date1 = new Date("01-14-2023")
+console.log(myNewDate.toLocaleString);
+console.log(Date1.toLocaleString());
+
